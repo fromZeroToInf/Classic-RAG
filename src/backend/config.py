4a41440b03bb00184e784912213ee07f1dacd40e
Shin-Young Hur <fromZeroToInf@outlook.de>
2026-08-12 13:28:49 +0200

    github actions workflow created

--- src/backend/config.py
+++ src/backend/config.py
@@ -1,16 +1,15 @@
-from pydantic_settings import BaseSettings, SettingsConfigDict
-from pydantic import Field
 from pathlib import Path
+
+from pydantic import Field
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
 from common.getprojectroot import define_project_root_path
 
+
 class Settings(BaseSettings):
-    model_config = SettingsConfigDict(env_file=str(define_project_root_path() /".env"))
+    model_config = SettingsConfigDict(env_file=str(define_project_root_path() / ".env"))
     QDRANT_HOST: str = Field(default="localhost")
     QDRANT_PORT: int = Field(default=6333)
-    DOCUMENTS_DIR: Path = Field(default=define_project_root_path()/"data/input_docs")
-    LLM_PROVIDER: str|None = Field(default=None)
-    LLM_API_KEY: str| None = Field(default=None)
-    
-    
-    
-    
+    DOCUMENTS_DIR: Path = Field(default=define_project_root_path() / "data/input_docs")
+    LLM_PROVIDER: str | None = Field(default=None)
+    LLM_API_KEY: str | None = Field(default=None)
--- src/common/getprojectroot.py
+++ src/common/getprojectroot.py
@@ -1,13 +1,14 @@
 import sys
 from pathlib import Path
 
+
 def define_project_root_path():
     toFind = ("pyproject.toml", "setup.py", ".git")
-    
+
     cwd = Path.cwd().resolve()
-    
+
     for folder in (cwd, *cwd.parents):
-        if any((folder / file).exists() for file in toFind ):
+        if any((folder / file).exists() for file in toFind):
             sys.path.insert(0, str(folder))
             return folder
     raise FileNotFoundError(f"Could not find project root: {cwd}")
